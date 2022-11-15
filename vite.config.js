9ff4f93913533f7815ba5b5ec18ff4b287a48ed9
liuhao1234 <728672190@qq.com>
2022-11-15 17:23:33 +0800

    feat: 配置优化

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// https://vitejs.dev/config/
+// https://vitejs.cn/
 export default defineConfig({
   envDir: "./viteEnv",
   envPrefix: ["VITE", "VENUS"], //这个时候，我们可以将VITE_、VENUS_开头的变量统统透传给客户端
